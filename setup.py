416e59a958ac9a8a165d13b8d355dac60c7328a9
Deryck Hodge <deryck@storybird.com>
2015-05-27 15:24:58 -0500

    setup.py fixes to cleanly install.

--- setup.py
+++ setup.py
@@ -5,12 +5,11 @@ setup(
     name='smartchoices',
     version='1.0',
     description='A smart choices interface for Django apps',
-    long_description=open('README.md', 'r').read(),
     author='Storybird Developers',
     author_email='dev@storybird.com',
     url='https://github.com/storybird/smartchoices',
     license='MIT',
-    packages=('smartchoices',),
+    modules=('smartchoices',),
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
